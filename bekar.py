fbfd2853cd9168ccf697f4f23efc58fefad89012
Nihat 「 FARZ 」 <99778063+NihatFarz@users.noreply.github.com>
2022-03-28 16:44:47 +0400

    「 FARZ 」

---
+++ bekar.py
@@ -0,0 +1,23 @@
+name=input("Adın Nədir ??\n ➡️ ➡️ ➡️")
+x=name.title()
+print(f"#1.Sənin Adın \nMR\t{x} ")
+print('_______________________')
+y=len(x)
+print(" 🐲 🐲 🐲 🐲 🐲 🐲 🐲 ")
+print(f"#2.Adının uzunu ;D {y}")
+print("#########################")
+print(f"#3.Adının Tərsi (Bekarçılıqdandı {x[-1::-1]}")
+age =input("Neçə yaşın var? \n==> ")
+age=int(age)
+if age>=18:
+	print("18+ olmusan a manıs 😎")
+else:
+	print("Hələ 18 yaşın olmuyub \n Yaşın Çatmır qed o tərəfə 👺")
+print('Tanımadıqın insanlara inanma Dostum  ')
+x=input('mənə inanarsan?? \nhə/yox   ::>')
+if x=='yox':
+	while True:
+		print("Sistemini Ələ keçirdim..")
+		print("Məlumatlarını yükləyirəm..")
+elif x=='hə':
+	print('Bu sadəcə bir zarafat idi Dostum narahat olma .\n\nTelegram=> ✓✓✓✓✓https://t.me/NihatFarz')
